6572b4be33529c087f839fc699c3db1fcf9f3642
nodeexcel <atul.etech2011@gmail.com>
2024-07-23 18:46:16 +0530

    added adming functionality route and controller

---
+++ src/routes/route.admin.ts
@@ -0,0 +1,13 @@
+import express, { Router } from 'express'
+import { adminController } from '../controllers/admins'
+import { fileUpload } from '../utils/util.upload'
+import { adminValidator, paramsValiator } from '../utils/util.validator'
+import { roleJwt } from '../middlewares/middleware.role'
+
+const router: Router = express.Router()
+
+router.post('/admin', [...adminValidator(), fileUpload.fields([{ name: 'photo' }])], adminController.createAdmin)
+router.get('/admin', roleJwt(), adminController.resultsAdmin)
+router.get('/admin/:id', [roleJwt(), ...paramsValiator()], adminController.resultAdmin)
+
+export default router
